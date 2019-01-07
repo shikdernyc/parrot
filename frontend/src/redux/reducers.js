aa2ac12187b0615ab1a1693be8c005b3fcaadbbe
shikdernyc <shikdernyc@gmail.com>
2019-01-07 15:36:32 -0500

    Added app, domains, intents and nav reducers

--- frontend/src/redux/reducers.js
+++ frontend/src/redux/reducers.js
@@ -1,8 +1,14 @@
-import { combineReducers } from 'redux';
-import app from './app/reducer';
+import { combineReducers } from "redux";
+import app from "./app/reducer";
+import domains from "./domains/reducer";
+import intents from "./intents/reducer";
+import navs from './navs/reducer'
 
 const reducers = combineReducers({
-  app
+  app,
+  domains,
+  intents,
+  navs
 });
 
 export default reducers;
