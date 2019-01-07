6281ef158dfd36f45c04d1eb37aef3a3f51d6ed8
shikdernyc <shikdernyc@gmail.com>
2019-01-07 15:35:20 -0500

    Added state and actions for setting top nav props and setting sideBarToBeOpen

---
+++ frontend/src/redux/navs/action.js
@@ -0,0 +1,15 @@
+import {SET_SIDENAV_ISOPEN, SET_TOP_NAV_PROPS} from '../../constants/actionTypes'
+
+export function setSideBarIsOpen(sideBarIsOpen){
+    return{
+        type: SET_SIDENAV_ISOPEN,
+        sideBarIsOpen
+    }
+}
+
+export function setTopNavProps(props){
+    return {
+        type: SET_TOP_NAV_PROPS,
+        props
+    }
+}
