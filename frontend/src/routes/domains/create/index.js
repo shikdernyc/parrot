73f7fc34aeb385a0609620840333586622e4454e
shikdernyc <shikdernyc@gmail.com>
2019-01-07 15:46:19 -0500

    Added placeholder for create forms

---
+++ frontend/src/routes/domains/create/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Create extends Component{
+    render(){
+        return(
+            <h1>Hello, World</h1>
+        )
+    }
+}
+
+export default Create
