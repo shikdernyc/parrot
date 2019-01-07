04b149a41bdbdffbb08c23e8c8e477481e0d3e78
shikdernyc <shikdernyc@gmail.com>
2019-01-07 15:26:48 -0500

    Added constants for app and action types

---
+++ frontend/src/constants/actionTypes.js
@@ -0,0 +1,7 @@
+// navs
+export const SET_TOP_NAV_PROPS = "SET_TOP_NAV_PROPS"
+export const SET_TOP_NAV_TYPE = "SET_TOP_NAV_TYPE"
+export const SET_SIDENAV_ISOPEN = "SET_SIDENAV_ISOPEN"
+
+// domains
+export const ADD_TO_DOMAIN_LIST = "ADD_TO_DOMAIN_LIST"
