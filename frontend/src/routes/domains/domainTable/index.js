bfa79688df7968cbe8171dbd3c2e7080b34bfb7b
shikdernyc <shikdernyc@gmail.com>
2019-01-07 15:41:00 -0500

    Added domain list route and create root

---
+++ frontend/src/routes/domains/domainTable/index.js
@@ -0,0 +1,91 @@
+import React, { Fragment, Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import { connect } from "react-redux";
+import Paper from "@material-ui/core/Paper";
+import AutosizerTable from "../../../components/table/AutosizerTable";
+import { setTopNavProps } from "../../../redux/navs/action";
+
+class DomainTable extends Component {
+  componentDidMount(){
+    this.props.setNavButton()
+  }
+
+  render() {
+    let { classes, rows } = this.props;
+    rows = rows.map(item => ({
+      ...item,
+      enabled: item.enabled ? "Yes" : "No"
+    }));
+    return (
+      <Fragment>
+        <Paper className={classes.root}>
+          <AutosizerTable
+            rowCount={rows.length}
+            rowGetter={({ index }) => rows[index]}
+            onRowClick={event => {
+              this.props.history.push(`/domains/${event.rowData.id}`);
+            }}
+            columns={[
+              {
+                width: 300,
+                flexGrow: 1.0,
+                label: "Domain Name",
+                dataKey: "domainName"
+              },
+              {
+                width: 250,
+                label: "Enabled",
+                dataKey: "enabled"
+              },
+              {
+                width: 250,
+                label: "Intent Threshold",
+                dataKey: "threshold"
+              }
+            ]}
+          />
+        </Paper>
+      </Fragment>
+    );
+  }
+}
+
+const mapStateToProps = function(reduxState) {
+  return {
+    rows: reduxState.domains.domainList
+  };
+};
+
+const mapDispatchToProps = function(dispatch) {
+  return {
+    setNavButton: function(
+      buttonText = "Create Domain",
+      buttonLink = "/domain/create"
+    ) {
+      dispatch(
+        setTopNavProps({
+          buttonText,
+          buttonLink
+        })
+      );
+    }
+  };
+};
+
+const domainTableStyles = theme => ({
+  root: {
+    width: "100%",
+    height: window.innerHeight * 0.65,
+    marginTop: theme.spacing.unit * 3
+  }
+});
+
+DomainTable.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withStyles(domainTableStyles)(DomainTable));
--- frontend/src/routes/domains/index.js
+++ frontend/src/routes/domains/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
-import DomainList from "./domainList";
-
+import DomainList from "./domainTable/index";
+import Create from './create'
 export default ({ match }) => {
   return (
     <Switch>
-      <Route path={`${match.url}`} component={DomainList} />
+      <Route exact path={`${match.url}`} component={DomainList} />
+      <Route exact path={`${match.url}/create`} component={Create} />
       <Redirect to="/error/404" />
     </Switch>
   );
