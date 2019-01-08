814563b9b74ba0debad662276858ab6f17b84c79
shikdernyc <shikdernyc@gmail.com>
2019-01-08 17:26:25 -0500

    Added agents reducer with hardcoded agentList

--- frontend/src/redux/reducers.js
+++ frontend/src/redux/reducers.js
@@ -3,9 +3,11 @@ import app from "./app/reducer";
 import domains from "./domains/reducer";
 import intents from "./intents/reducer";
 import navs from './navs/reducer'
+import agents from './agents/reducer'
 
 const reducers = combineReducers({
   app,
+  agents,
   domains,
   intents,
   navs
