e5575c1ff1539ef929a9c02efd34182fc4a7491d
shikdernyc <shikdernyc@gmail.com>
2019-01-08 17:29:15 -0500

    updated theme

--- frontend/src/withRoot.js
+++ frontend/src/withRoot.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
+// import indigo	 from '@material-ui/core/colors/indigo	';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 // A theme with custom primary and secondary color.
@@ -9,14 +9,14 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      light: purple[300],
-      main: purple[500],
-      dark: purple[700],
+      // light: purple[300],
+      main: purple[500]
+      // dark: purple[700],
     },
     secondary: {
-      light: green[300],
-      main: green[500],
-      dark: green[700],
+      // light: green[300],
+      main: '#e6e6fa',
+      // dark: green[700],
     },
   },
   typography: {
