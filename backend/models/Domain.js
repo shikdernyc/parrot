e29a8081d4c709dea4b4597e9e29d59c1f56d3cd
Leon Feng <best.sum@gmail.com>
2019-01-09 14:21:09 -0500

    add models

---
+++ backend/models/Domain.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const DomainSchema = new mongoose.Schema({
+  id: Number,
+  agent: {
+    type: String,
+    trim: true
+  },
+  domainName: {
+    type: String,
+    trim: true
+  },
+  enabled: Boolean,
+  intentThreshold: Number,
+  status: {
+    type: String,
+    trim: true
+  },
+  lastTraining: Date,
+  model: {
+    type: String,
+    trim: true
+  },
+  extraTrainingData: Boolean
+});
+
+const Domain = mongoose.model('Domain', DomainSchema);
+
+module.exports = Domain;
---
+++ backend/models/Intent.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const ExampleIntent = require('./ExampleIntent');
+
+const IntentSchema = new mongoose.Schema({
+  id: Number,
+  agent: {
+    type: String,
+    trim: true
+  },
+  domain: {
+    type: String,
+    trim: true
+  },
+  intentName: {
+    type: String,
+    trim: true
+  },
+  examples: [ExampleIntent.schema],
+  useWebhook: Boolean,
+  usePostFormat: Boolean
+});
+
+const Intent = mongoose.model('Intent', IntentSchema);
+
+module.exports = Intent;
