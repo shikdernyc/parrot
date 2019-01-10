6267ef9c023a536d939027e5ab0a51e4cfbcb0d5
Leon Feng <best.sum@gmail.com>
2019-01-10 15:58:57 -0500

    add route for intent

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 
 const AgentRouter = require('./agent');
 const DomainRouter = require('./domain');
+const IntentRouter = require('./intent');
 const RootRouter = require('./root');
 
 router.use('/', RootRouter);
 router.use('/agents', AgentRouter);
 router.use('/agents/:agent_id/domains', DomainRouter);
+router.use('/agents/:agent_id/domains/:domain_id', IntentRouter);
 
 module.exports = router;
