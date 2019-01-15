4ddcfcbf2c08a844b85a7fc6e2594962f48e6dc6
shikdernyc <shikdernyc@gmail.com>
2019-01-15 16:24:08 -0500

    Added abstraction for interaction with the server

---
+++ frontend/src/services/server.js
@@ -0,0 +1,34 @@
+import Axios from 'axios';
+import { SERVER_URL } from 'Constants/app';
+
+export const get = async function (route) {
+  try {
+    return await Axios.get(`${SERVER_URL}/${route}`);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const post = async function (route, data) {
+  try {
+    return await Axios.post(`${SERVER_URL}/${route}`, data);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const put = async function (route, data) {
+  try {
+    return await Axios.put(`${SERVER_URL}/${route}`, data);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const remove = async function (route) {
+  try {
+    return await Axios.delete(`${SERVER_URL}/${route}`);
+  } catch (error) {
+    throw error;
+  }
+};
