651e4ce93e85c1e8053c81f80d0b5f17fa346a32
shikdernyc <shikdernyc@gmail.com>
2019-01-25 11:57:20 -0500

    Added intent saga

--- frontend/src/redux/sagas.js
+++ frontend/src/redux/sagas.js
@@ -2,7 +2,8 @@ import { all } from 'redux-saga/effects';
 import app from './app/saga';
 import agents from './agents/saga';
 import domains from './domains/saga';
+import intents from './intents/sagas';
 
 export default function * rootSaga (getState) {
-  yield all([app(), agents(), domains()]);
+  yield all([app(), agents(), domains(), intents()]);
 }
