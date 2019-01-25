be6c7b007e468c07f7a91b30c5b033b43423ef16
shikdernyc <shikdernyc@gmail.com>
2019-01-24 21:33:12 -0500

    Added actions for create, setDomainListAgent and updateDomainList

--- frontend/src/redux/domains/actions.js
+++ frontend/src/redux/domains/actions.js
@@ -1,8 +1,38 @@
-import { ADD_TO_DOMAIN_LIST } from 'Constants/actionTypes';
+import {
+  ADD_TO_DOMAIN_LIST,
+  UPDATE_DOMAIN_LIST,
+  SET_DOMAIN_LIST_AGENT,
+  CREATE_DOMAIN
+} from 'Constants/actionTypes';
+
+export function createDomain (domainSchema) {
+  return {
+    type: CREATE_DOMAIN,
+    payload: { domainSchema }
+  };
+}
 
 export function addToDomainList (domain) {
   return {
     type: ADD_TO_DOMAIN_LIST,
     domain
+  };
+}
+
+export function setDomainListAgent (agentID) {
+  return {
+    type: SET_DOMAIN_LIST_AGENT,
+    payload: {
+      agentID
+    }
+  };
+}
+
+export function updateDomainList (newDomainList) {
+  return {
+    type: UPDATE_DOMAIN_LIST,
+    payload: {
+      newDomainList
+    }
   };
 }
