bfaad6b9ac4d6d7fe5bbab7c3a9f79aa68a6fd83
shikdernyc <shikdernyc@gmail.com>
2019-01-24 21:36:07 -0500

    added domain sagas

--- frontend/src/redux/sagas.js
+++ frontend/src/redux/sagas.js
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
 import app from './app/saga';
 import agents from './agents/saga';
+import domains from './domains/saga';
 
 export default function * rootSaga (getState) {
-  yield all([app(), agents()]);
+  yield all([app(), agents(), domains()]);
 }
