1021c27c4cd81834eedecf3fc92fc90603d8179f
shikdernyc <shikdernyc@gmail.com>
2019-01-27 12:13:38 -0500

    Added dashboard

---
+++ frontend/src/routes/agents/dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+
+class Dashboard extends Component {
+  render () {
+    return (
+      <Fragment>
+        <h2>Agent Landing Page</h2>
+        <p>Select a domain to get started</p>
+      </Fragment>
+    );
+  }
+}
+
+export default Dashboard;
--- frontend/src/routes/agents/index.js
+++ frontend/src/routes/agents/index.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
 import NewAgentForm from './NewAgentForm';
+import Dashboard from './dashboard';
 
 const Agent = ({ match }) => {
   return (
     <Switch>
       <Route exact path={`${match.url}/create`} component={NewAgentForm} />
       <Route exact path={`${match.url}/dashboard`} component={NewAgentForm} />
+      <Route exact path={`${match.url}`} component={Dashboard} />
       <Redirect to="/error/404" />
     </Switch>
   );
