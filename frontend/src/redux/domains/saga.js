b2e7d7abd25cfeff662c6b081153f5f465903bc5
shikdernyc <shikdernyc@gmail.com>
2019-01-27 12:10:43 -0500

    Added set current domain

--- frontend/src/redux/domains/saga.js
+++ frontend/src/redux/domains/saga.js
@@ -1,8 +1,15 @@
-import { getAllDomains } from 'Data/models/Agent';
-import { create } from 'Data/models/Domain';
+import { create, findById, getAllDomains } from 'Data/models/Domain';
 import { all, call, fork, put, takeEvery } from 'redux-saga/effects';
-import { SET_DOMAIN_LIST_AGENT, CREATE_DOMAIN } from 'Constants/actionTypes.js';
-import { updateDomainList, addToDomainList } from './actions';
+import {
+  SET_DOMAIN_LIST_AGENT,
+  CREATE_DOMAIN,
+  SET_CURRENT_DOMAIN
+} from 'Constants/actionTypes.js';
+import {
+  updateDomainList,
+  addToDomainList,
+  updateCurrentDomain
+} from './actions';
 
 function * handleCreateDomain ({ domainSchema, history }) {
   try {
@@ -19,8 +26,29 @@ function * handleSetDomainListAgent ({ payload: { agentID } }) {
   try {
     const domains = yield call(getAllDomains, agentID);
     yield put(updateDomainList(domains));
+    if (domains.length === 0) {
+      throw new Error('Empty domain list');
+    } else {
+      // yield put(setCurrentDomain(domains[0]._id));
+    }
   } catch (error) {
-    console.log(error);
+    // TODO: Handle empty domain list error
+    // TODO: handle unable to retrieve domain list error
+    console.log(error.message);
+    throw error;
+  }
+}
+
+function * handleSetCurrentDomain ({ payload: { id, history } }) {
+  try {
+    const domain = yield call(findById, id);
+    yield put(updateCurrentDomain(domain));
+    // handle if history is passed
+    if (history) {
+      console.log('History received');
+    }
+  } catch (error) {
+    // TODO: Handle failure
     throw error;
   }
 }
@@ -33,6 +61,14 @@ export function * watchSetDomainListAgent () {
   yield takeEvery(SET_DOMAIN_LIST_AGENT, handleSetDomainListAgent);
 }
 
+export function * watchSetCurrentDomain () {
+  yield takeEvery(SET_CURRENT_DOMAIN, handleSetCurrentDomain);
+}
+
 export default function * rootSaga () {
-  yield all([fork(watchCreateDomain), fork(watchSetDomainListAgent)]);
+  yield all([
+    fork(watchCreateDomain),
+    fork(watchSetDomainListAgent),
+    fork(watchSetCurrentDomain)
+  ]);
 }
